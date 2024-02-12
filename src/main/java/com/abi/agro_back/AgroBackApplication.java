cfd8eaab8ace418edbe1e292bdd478bcb9e0fc91
styopa-91 <styopaxxl@gmail.com>
2024-02-12 09:33:53 +0200

    initialize spring boot project

---
+++ src/main/java/com/abi/agro_back/AgroBackApplication.java
@@ -0,0 +1,13 @@
+package com.abi.agro_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroBackApplication.class, args);
+	}
+
+}
